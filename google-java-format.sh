f9700602a4f83c5ba80fa01e8758ec595ee5b696
mihai.maier <mihai.maier1991@gmail.com>
2022-12-16 15:04:21 +0200

    change script

--- google-java-format.sh
+++ google-java-format.sh
@@ -3,8 +3,7 @@ mkdir -p .cache
 cd .cache
 if [ ! -f google-java-format-1.15.0-all-deps.jar ]
 then
-    curl -LJO "https://github.com/google/google-java-format/releases/download/v1.15.0/google-java-format-1.15.0-all-deps.jar"
-    chmod 755 google-java-format-1.15.0-all-deps.jar
+curl "https://github.com/google/google-java-format/releases/download/v1.15.0/google-java-format-1.15.0-all-deps.jar" -o google-java-format-1.15.0-all-deps.jar    
 fi
 cd ..
 
