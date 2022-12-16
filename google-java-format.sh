5ab43087231b8a088b1af3e1ab0c51f388d8b246
mihai.maier <mihai.maier1991@gmail.com>
2022-12-16 15:23:13 +0200

    change script

--- google-java-format.sh
+++ google-java-format.sh
@@ -1,12 +1,35 @@
 #!/usr/bin/env sh
-mkdir -p .cache
-cd .cache
-if [ ! -f google-java-format-1.15.0-all-deps.jar ]
-then
-curl "https://github.com/google/google-java-format/releases/download/v1.15.0/google-java-format-1.15.0-all-deps.jar" -o google-java-format-1.15.0-all-deps.jar    
-fi
-cd ..
 
-changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" )
-echo $changed_java_files
-java -jar .cache/google-java-format-1.15.0-all-deps.jar -c ./formatter.cfg --replace $changed_java_files
+RELEASE=1.15.0
+JAR_NAME="google-java-format-${RELEASE}-all-deps.jar"
+RELEASES_URL=https://repo1.maven.org/maven2/com/google/googlejavaformat/google-java-format
+JAR_URL="${RELEASES_URL}/${RELEASE}/${JAR_NAME}"
+
+CACHE_DIR="$HOME/.cache"
+JAR_FILE="$CACHE_DIR/$JAR_NAME"
+
+FORMATER_CONF=formatter.cfg
+
+if [[ ! -f "$JAR_FILE" ]]
+then
+    mkdir -p "$CACHE_DIR"
+    curl -L "$JAR_URL" -o "$JAR_NAME"
+fi
+
+changed_java_files=$(git diff --cached --name-only --diff-filter=ACMR | grep ".*java$" || true)
+if [[ -n "$changed_java_files" ]]
+then
+    echo "Reformatting Java files: $changed_java_files"
+    if ! java   --add-exports jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED \
+                --add-exports jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
+                --add-exports jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED \
+                --add-exports jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED \
+                --add-exports jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED \
+                -jar "$JAR_FILE" -c ./$FORMATER_CONF --replace --set-exit-if-changed $changed_java_files
+    then
+        echo "An error occurred, aborting commit!" >&2
+        exit 1
+    fi
+else
+    echo "No Java files changes found."
+fi
